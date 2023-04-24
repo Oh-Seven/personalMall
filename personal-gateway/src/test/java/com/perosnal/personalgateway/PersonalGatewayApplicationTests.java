ca44381dd09837c151778c486138da196f2735b0
lilei <lilei-3@thunisoft.com>
2023-04-24 16:14:49 +0800

    前端代码

---
+++ personal-gateway/src/test/java/com/perosnal/personalgateway/PersonalGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.perosnal.personalgateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalGatewayApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
